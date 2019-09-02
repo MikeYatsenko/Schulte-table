65fb999fab6e789d58a782e2983f2f99f3003959
HarryVorobyov <harryvorobyov@gmail.com>
2019-09-02 23:18:18 +0300

    pref Activity

--- app/src/main/java/com/e/schultetable/MainActivity.java
+++ app/src/main/java/com/e/schultetable/MainActivity.java
@@ -1,5 +1,6 @@
 package com.e.schultetable;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -91,7 +92,8 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_home) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
+            Intent intent = new Intent(this, PrefActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_tools) {
